b5e2327b92b2f2a8ea60d8d14a16103f7e8cb355
Trevor Buchanan <trevor@trevorbuchanan.com>
2020-08-31 18:46:12 -0500

    removed "useless construtors" so netlify build would complete

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,9 +2,6 @@ import React from 'react';
 
 
 class Todo extends React.Component {
-  constructor(props) {
-    super(props)
-  }
   render() {
     return (
       <p className={this.props.elem.completed ? 'strike' : '' }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,9 +4,6 @@ import React from 'react';
 import Todo from './Todo';
 
 class TodoList extends React.Component {
-  constructor(props) {
-    super(props)
-  }
 
   render() {
     return (
